1a7b0f0dd60719e4250aee8c9c9d2e8c6eeb0754
Keshav Mohta <xkeshav@gmail.com>
2023-07-31 15:03:00 +0530

    added build script

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -18,8 +18,8 @@ const config = (_, argv) => {
     target: "node",
     node: {
       // Need this when working with express, otherwise the build fails
-      __dirname: false, // if you don't put this is, __dirname
-      __filename: false, // and __filename return blank or /
+      __dirname: false, // if you don't put this is, __dirname and __filename return blank or /
+      __filename: false,
     },
     externals: [nodeExternals()], // Need this to avoid error when working with Express
     module: {
